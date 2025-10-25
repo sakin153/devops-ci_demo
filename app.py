b961ab367336463f5cb83614542f711f4ae43c13
Sakin Abdullaev <sakin153@bk.ru>
2025-10-25 10:08:39 +0300

    added task

---
+++ app.py
@@ -0,0 +1,8 @@
+from utils import add_numbers
+
+def main():
+    result = add_numbers(2, 3)
+    print(f"Результат сложения: {result}")
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,3 @@
+def add_numbers(a: int, b: int) -> int:
+    """Возвращает сумму двух чисел."""
+    return a + b
